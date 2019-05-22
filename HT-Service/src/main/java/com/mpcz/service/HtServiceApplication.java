b7b988b9ad359d2c764100e29ef74cd2f371ecc6
AMIT PATERIYA <amitpateriya.1989@gmail.com>
2019-05-22 15:08:05 +0530

    First HT-Billing Service commit

---
+++ HT-Service/src/main/java/com/mpcz/service/HtServiceApplication.java
@@ -0,0 +1,13 @@
+package com.mpcz.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HtServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HtServiceApplication.class, args);
+	}
+
+}
