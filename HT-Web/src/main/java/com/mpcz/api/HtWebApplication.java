173db767e3bef87fc30bd9f1e88eddb9bea5befe
amitpateriya1989 <amitpateriya.1989@gmail.com>
2019-05-22 16:42:49 +0530

    Add files via upload
    HT Billing Web First Commit

---
+++ HT-Web/src/main/java/com/mpcz/api/HtWebApplication.java
@@ -0,0 +1,17 @@
+package com.mpcz.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class HtWebApplication {
+
+
+	
+		public static void main(String[] args) {
+			SpringApplication.run(HtWebApplication.class, args);
+		}
+
+	
+}
