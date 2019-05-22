bd356142fa6c2bab5387d7a41e7e93156bcd4e69
AMIT PATERIYA <amitpateriya.1989@gmail.com>
2019-05-22 15:13:30 +0530

    HT-Repository first commit

---
+++ HT-Repository/src/main/java/com/mpcz/repository/HtRepositoryApplication.java
@@ -0,0 +1,13 @@
+package com.mpcz.repository;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HtRepositoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HtRepositoryApplication.class, args);
+	}
+
+}
